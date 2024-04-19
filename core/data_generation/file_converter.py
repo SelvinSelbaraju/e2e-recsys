4a675f7f48db5667e86b82d483b28ceb703887c4
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-04-19 22:51:40 +0100

    create fileconverter

---
+++ core/data_generation/file_converter.py
@@ -0,0 +1,51 @@
+import os
+import traceback
+from typing import List, Optional
+import pandas as pd
+import torch
+
+class FileConverter:
+    def __init__(self, columns: List[str], input_filepath: str, output_dir: str, output_file_prefix: str = "data_row", file_extension: str = "pt"):
+        self.columns = columns
+        self.input_filepath = input_filepath
+        self.output_dir = output_dir
+        self.output_file_prefix = output_file_prefix
+        self.file_extension = file_extension
+
+        self.data_loader = pd.read_csv(self.input_filepath, usecols=self.columns, chunksize=1)
+        
+        # Keep track of which row has been loaded 
+        self.current_row_idx = 0
+
+        # Make output dir if does not exist
+        os.makedirs(self.output_dir, exist_ok=True)
+
+    def _load_row(self) -> pd.DataFrame:
+        self.row = next(self.data_loader).to_numpy()
+    
+    def _save_row(self):
+        output_filename = f"{self.output_file_prefix}_{self.current_row_idx}.{self.file_extension}"
+        output_path = os.path.join(self.output_dir, output_filename)
+        torch.save(self.row, output_path)
+    
+    def convert_rows(self, max_row_idx: Optional[int] = None):
+        if max_row_idx:
+            for _ in range(max_row_idx):
+                try:
+                    self._load_row()
+                    self._save_row()
+                    # Move to the next row
+                    self.current_row_idx += 1
+                except Exception:
+                    traceback.print_exc()
+
+
+
+# converter = FileConverter(
+#     ["article_id", "prod_name"],
+#     "/Users/selvino/e2e-recsys/data/articles.csv",
+#     "converted_data",
+# )
+
+# # converter.convert_rows(2)
+
