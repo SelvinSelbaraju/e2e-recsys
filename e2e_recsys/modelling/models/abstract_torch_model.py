5ac08a9980dde22ad7ef6113d00aa472b1eb125f
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-04-21 11:56:05 +0100

    create abstract class

---
+++ e2e_recsys/modelling/models/abstract_torch_model.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+import torch
+
+
+class AbstractTorchModel(ABC, torch.nn.Module):
+    default_activation = "Relu"
+    default_output_transform = "Sigmoid"
+
+    def __init__(self, model_config: Dict):
+        self.model_config = model_config
+        self._init_functions()
+
+    # Set activation functions and output layer transformation
+    def _init_functions(self) -> None:
+        module = torch.nn
+        self.activation = getattr(
+            module,
+            self.model_config.get("activation", self.default_activation),
+        )()
+        self.output_transform = getattr(
+            module,
+            self.model_config.get(
+                "output_transform", self.default_output_transform
+            ),
+        )()
+
+    @abstractmethod
+    def _init_layers(self) -> None:
+        pass
+
+    # This should be called in _init_layers
+    @abstractmethod
+    def _init_categorical_layers(self) -> None:
+        pass
+
+    @abstractmethod
+    def forward(self, x) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def _preprocessing(self, x) -> Dict[str, torch.Tensor]:
+        pass
