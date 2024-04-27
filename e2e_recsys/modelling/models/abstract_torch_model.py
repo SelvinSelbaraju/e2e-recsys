c66ad330771e38c1152b8f5943b905c25bda8dc3
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-04-27 06:48:44 +0100

    rebuild abstract pytorch class

--- e2e_recsys/modelling/models/abstract_torch_model.py
+++ e2e_recsys/modelling/models/abstract_torch_model.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, Set
 import torch
 
 
@@ -7,8 +7,16 @@ class AbstractTorchModel(ABC, torch.nn.Module):
     default_activation = "Relu"
     default_output_transform = "Sigmoid"
 
-    def __init__(self, model_config: Dict):
-        self.model_config = model_config
+    def __init__(
+        self,
+        hyperparam_config: Dict,
+        numeric_feature_names: Set[str],
+        categorical_feature_names: Set[str],
+    ):
+        self.hyperparam_config = hyperparam_config
+        self.numeric_feature_names = numeric_feature_names
+        self.categorical_feature_names = categorical_feature_names
+
         self._init_functions()
         self._init_layers()
 
@@ -17,11 +25,11 @@ class AbstractTorchModel(ABC, torch.nn.Module):
         module = torch.nn
         self.activation = getattr(
             module,
-            self.model_config.get("activation", self.default_activation),
+            self.hyperparam_config.get("activation", self.default_activation),
         )()
         self.output_transform = getattr(
             module,
-            self.model_config.get(
+            self.hyperparam_config.get(
                 "output_transform", self.default_output_transform
             ),
         )()
@@ -31,14 +39,12 @@ class AbstractTorchModel(ABC, torch.nn.Module):
         pass
 
     # This should be called in _init_layers
+    # Categorical layers are assigned to integers during data preprocessing
+    # This is because PyTorch does not handle strings well
     @abstractmethod
     def _init_categorical_layers(self) -> None:
         pass
 
     @abstractmethod
     def forward(self, x) -> torch.Tensor:
-        pass
-
-    @abstractmethod
-    def _preprocessing(self, x) -> Dict[str, torch.Tensor]:
         pass
